715026fe38f9529d1c1a1926d0a504788f327ab8
Prajwal Athare <151899432+Praj1703@users.noreply.github.com>
2025-01-24 10:13:34 +0530

    Add files via upload

--- BST.cpp
+++ BST.cpp
@@ -10,6 +10,7 @@ class BST
 			Node *lc,*rc;
 		}*root;
 	public:
+	
 	BST()
 	{
 		root=NULL;
@@ -18,7 +19,10 @@ class BST
 	void rec_preorder(Node *root);
 	void rec_inorder(Node *root);
 	void rec_postorder(Node *root);
+	void findMin(Node *root);
+	void displaymin();
 	void create()
+		
 	{
 		Node *temp,*curr;
 		int ch;
@@ -70,15 +74,35 @@ class BST
 }
 
 };
+int BST::findMin(Node* root){
+	if (root == nullptr) {
+        throw "Tree is empty";
+    }
 
+    Node* current = root;
+    while (current->lc != nullptr) {
+        current = current->lc;
+    }
+    return current->data;
+}
+void BST::displaymin(){
+	cout<<"Display the minimum Value "<<endl;
+	findMin(Node* root);
+}
 
 void BST::dis()
 {
-	rec_preorder(root);
-	cout<<"Inorder Traversal: "<<endl;
-	rec_inorder(root);
-	cout<<"Postorder Traversal: "<<endl;
-	rec_postorder(root);
+	cout<<"*********************************************"<<endl;
+		cout<<"Pre-Order Traversal :"<<endl;
+		rec_preorder(root);
+		cout<<"\n";
+		cout<<"Inorder Traversal: "<<endl;
+		rec_inorder(root);
+		cout<<"\n";
+		cout<<"Postorder Traversal: "<<endl;
+		rec_postorder(root);
+		cout<<"\n\n";
+	cout<<"*********************************************"<<endl;
 }
 
 void BST::rec_preorder(Node *t)
@@ -88,7 +112,7 @@ void BST::rec_preorder(Node *t)
 		cout<<"\t"<<t->data;
 		rec_preorder(t->lc);
 		rec_preorder(t->rc);
-		cout<<""<<endl;
+
 	}
 }
 void BST::rec_inorder(Node *t)
@@ -97,17 +121,52 @@ void BST::rec_inorder(Node *t)
 	{
 		rec_inorder(t->lc);
 		cout<<"\t"<<t->data;
-		
 		rec_inorder(t->rc);
 
 	}
 }
+void BST::rec_postorder(Node *t)
+{
+	if(t!=NULL)
+	{
+		rec_postorder(t->lc);
+		rec_postorder(t->rc);
+		cout<<"\t"<<t->data;
+	}
+}
 int main()
 {
+	int choice; 
 	BST tree;
-	tree.create();
-	cout<<"Pre-Order Traversal :"<<endl;
-	tree.dis();	
-	cout<<""<<endl;
+
+	while(true){
+		cout<<"x-----------MENU-----------x"<<endl;
+		cout<<"|1. create BST             |"<<endl;
+		cout<<"|2. Display BST            |"<<endl;
+		cout<<"|3. Find Minimum           |"<<endl;
+		cout<<"|4. Exit                   |"<<endl;
+		cout<<"x--------------------------x"<<endl;
+		cout<<"Enter Your Choice : ";
+		cin>>choice;
+		cout<<"\n"<<endl;
+		
+		switch(choice){
+		case 1 :
+			tree.create();
+			break;
+		case 2 :
+			tree.dis();
+			break;
+		case 3 :
+ 			tree.displaymin();	
+		case 4 :
+			cout<<"Thank You for using the program"<<endl;
+			exit(0);
+		default:
+			cout<<"Invalid Input"<<endl;
+			break;
+	}
+	}
+	
 	return 0;
 }
